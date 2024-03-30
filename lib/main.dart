b89b36f29f1969ff602f8af0f8a530c4be29c5f1
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-03-30 11:49:29 +0500

    integrated firebase for iOS, android, web

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:delicious_food/pages/onboard_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBs8LFn1gjoxrNMJV5OVvgoKRdktZwKaZo",
+          authDomain: "quickfoodieapp-f38f4.firebaseapp.com",
+          projectId: "quickfoodieapp-f38f4",
+          storageBucket: "quickfoodieapp-f38f4.appspot.com",
+          messagingSenderId: "573866015021",
+          appId: "1:573866015021:web:fbdcbb616af0ad41047ba4",
+          measurementId: "G-BS3MXHJBSM"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -11,8 +30,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
-      title: 'Delicious Food',
+    return GetMaterialApp(
+      title: 'Quick Foodie',
       debugShowCheckedModeBanner: false,
       home: const OnBoardPage(),
       theme: ThemeData(useMaterial3: true),
